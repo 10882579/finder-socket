b8f5a1fdeab6e1d9fbec81ff6d0522073aa8f51e
Botir, Bahodirov <benjamin10882579@yahoo.com>
2019-08-23 03:34:08 -0700

    Initial push

---
+++ server.js
@@ -0,0 +1,18 @@
+const app = require('express')();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
+io.on('connection', (socket) => {
+  socket.on('chat-message', (data) => {
+    if(data.room){
+      const date = new Date(data.created_at);
+      io.emit(data.room, {
+        from: data.from,
+        message: data.message,
+        created_at: date.getTime()
+      })
+    }
+  })
+});
+
+server.listen(process.env.PORT || 3000);
