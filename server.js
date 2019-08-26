4eb8703397952c2d4d13db9b4651aef3b6cd96bb
Botir, Bahodirov <benjamin10882579@yahoo.com>
2019-08-26 03:54:02 -0700

    Notification @ initial stage

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 const app = require('express')();
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
+const bodyParser= require('body-parser');
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
+
+app.post('/notify/:token', (req, res) => {
+  io.emit(req.params.token, req.body);
+  res.send({}).status(200);
+})
 
 io.on('connection', (socket) => {
   socket.on('chat-message', (data) => {
